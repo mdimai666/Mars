52de1dd316203a47ff3df83364bf2f09451faac9
mdimai666 <mdimai666@mail.ru>
2025-07-05 11:45:58 +0900

    append files

--- src/Mars.Host.Shared/Features/FeatureFlags.cs
+++ src/Mars.Host.Shared/Features/FeatureFlags.cs
@@ -3,5 +3,6 @@ namespace Mars.Host.Shared.Features;
 public static class FeatureFlags
 {
     public const string DockerAgent = "DockerAgent";
+    public const string EShop = "EShop";
     public const string AITool = "AITool";
 }

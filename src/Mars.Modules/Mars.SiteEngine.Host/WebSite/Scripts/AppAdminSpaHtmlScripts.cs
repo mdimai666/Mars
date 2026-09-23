8ee72f0ce2062bf6769cb665de0cf77d94e065d6
mdimai666 <mdimai666@mail.ru>
2026-09-24 01:05:23 +0900

    refactor [nodes] make editor css self-contained: own varnode-badge, move execution-bar style into component, version nodes css assets

--- src/Mars.Modules/Mars.SiteEngine.Host/WebSite/Scripts/AppAdminSpaHtmlScripts.cs
+++ src/Mars.Modules/Mars.SiteEngine.Host/WebSite/Scripts/AppAdminSpaHtmlScripts.cs
@@ -48,8 +48,8 @@ public class AppAdminSpaHtmlScripts
             //new(new Uri(@"_content/mdimai666.Mars.Nodes.FormEditor/css/style.css", UriKind.Relative), placeInHead: true ,version:appVersion , order: defaultOrder),
 
             new(new Uri(@"css/style.css", UriKind.Relative), placeInHead: true, version:appVersion, order: defaultOrder),
-            new(new Uri(@"_content/Mars.Nodes.Workspace/styles.css", UriKind.Relative), placeInHead: true, order: defaultOrder),
-            new(new Uri(@"_content/mdimai666.Mars.Nodes.FormEditor/css/style.css", UriKind.Relative), placeInHead: true, order: defaultOrder),
+            new(new Uri(@"_content/Mars.Nodes.Workspace/styles.css", UriKind.Relative), placeInHead: true, version:appVersion, order: defaultOrder),
+            new(new Uri(@"_content/mdimai666.Mars.Nodes.FormEditor/css/style.css", UriKind.Relative), placeInHead: true, version:appVersion, order: defaultOrder),
             new(new Uri(@"Mars.Admin.styles.css", UriKind.Relative), placeInHead: true, order: defaultOrder),
         ];
 
